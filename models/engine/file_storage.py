ea28771c557532efed8b9f1040987192e7788c2b
Isaac <zuderg@gmail.com>
2021-12-12 15:50:30 -0600

    added close

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -60,3 +60,7 @@ class FileStorage:
         """This method deletes an entry"""
         if obj:
             self.__objects.pop(type(obj).__name__ + "." + str(obj.id))
+
+    def close(self):
+        """deserializing the JSON file to objects"""
+        self.reload()
