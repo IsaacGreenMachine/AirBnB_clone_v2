39aec500716779b72b40544056317547f37f8f6c
Isaac <zuderg@gmail.com>
2021-12-12 16:31:10 -0600

    fixed State

--- models/state.py
+++ models/state.py
@@ -14,24 +14,13 @@ class State(BaseModel, Base):
     name = Column(String(128), nullable=False)
 
     if environ['HBNB_TYPE_STORAGE'] == "db":
-        try:
-            cities = relationship("City", back_populates="state")
-        except Exception as ex:
-            @property
-            def cities(self):
-                """getter for cities"""
-                cities_list = models.storage.all(type(City))
-                matching_cities = []
-                for i in cities_list:
-                    if cities_list.get(i).state_id == self.id:
-                        matching_cities.append(cities_list.get(i))
-                return matching_cities
-
+        cities = relationship("City", back_populates="state")
+        
     else:
         @property
         def cities(self):
             """getter for cities"""
-            cities_list = models.storage.all(type(City))
+            cities_list = models.storage.all(City)
             matching_cities = []
             for i in cities_list:
                 if cities_list.get(i).state_id == self.id:
