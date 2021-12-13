68af80cd4662a7fe81f9d0754a062c194e0f76d2
Isaac <zuderg@gmail.com>
2021-12-13 11:57:47 -0600

    re-adding webflask

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""starts a Flask web application"""
+from logging import error
+from flask import Flask
+import flask
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """displays on index page"""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """displays on HBNH page"""
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def cRoute(text):
+    """displays on any child of /c"""
+    if '_' in text:
+        text = text.replace('_', ' ')
+    return 'C {}'.format(text)
+
+
+@app.route('/python')
+@app.route('/python/<text>')
+def opPy(text="is cool"):
+    """displays on any child of python with optional parameters"""
+    if '_' in text:
+        text = text.replace('_', ' ')
+    return 'Python {}'.format(text)
+
+
+# will run without using flask run. Now can run as a python file
+if __name__ == '__main__':
+    app.debug = True
+    app.run(debug=True, host="0.0.0.0", port=5000)
